2cad0bb77fb5856dced2269347136d5e028e915c
Anna-Khobta <ana14i88@gmail.com>
2023-05-06 17:52:06 +0300

    some refactoring

---
+++ src/delete-all/delete-all.service.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { DeleteAllRepository } from './delete-all.repository';
+
+@Injectable()
+export class DeleteAllService {
+  constructor(
+    @Inject(DeleteAllRepository)
+    protected deleteAllRepository: DeleteAllRepository,
+  ) {}
+
+  async deleteAll() {
+    try {
+      await this.deleteAllRepository.deleteAllBlogs();
+      await this.deleteAllRepository.deleteAllPosts();
+      await this.deleteAllRepository.deleteAllUsers();
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+}
