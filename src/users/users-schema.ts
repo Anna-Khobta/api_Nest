32da910064486c1be3e25d79cfacb8b6970853b8
Anna-Khobta <ana14i88@gmail.com>
2023-05-06 14:25:33 +0300

    users migration to nest

---
+++ src/users/users-schema.ts
@@ -0,0 +1,77 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class AccountDataSchema {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  login: string;
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  email: string;
+  @Prop({
+    required: true,
+  })
+  hashPassword: string;
+  @Prop({
+    required: true,
+  })
+  createdAt: string;
+}
+
+@Schema()
+export class EmailConfirmationClass {
+  @Prop({
+    required: true,
+  })
+  confirmationCode: string;
+  @Prop({
+    required: true,
+  })
+  expirationDate: Date;
+  @Prop({
+    required: true,
+  })
+  isConfirmed: boolean;
+}
+
+@Schema()
+export class PasswordRecoveryClass {
+  @Prop({
+    required: true,
+  })
+  recoveryCode: string;
+  @Prop({
+    required: true,
+  })
+  exp: Date;
+}
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+  })
+  accountData: AccountDataSchema;
+
+  @Prop({
+    required: true,
+  })
+  emailConfirmation: EmailConfirmationClass;
+
+  @Prop({
+    required: true,
+  })
+  passwordRecovery: PasswordRecoveryClass;
+
+  @Prop()
+  likesCount: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
