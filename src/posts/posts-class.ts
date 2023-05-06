f4c907577b53f1ec201c2ab60157724c84f04c02
Anna-Khobta <ana14i88@gmail.com>
2023-05-06 14:22:43 +0300

    migrate posts to nest (work)

---
+++ src/posts/posts-class.ts
@@ -0,0 +1,26 @@
+import { UserLikeInfo } from '../blogs/types';
+
+export class PostClassDbType {
+  createdAt: string;
+  likesCount: number;
+  dislikesCount: number;
+  usersEngagement: UserLikeInfo[];
+
+  constructor(
+    public title: string,
+    public shortDescription: string,
+    public content: string,
+    public blogId: string,
+    public blogName: string,
+  ) {
+    this.title = title;
+    this.shortDescription = shortDescription;
+    this.content = content;
+    this.blogId = blogId;
+    this.blogName = blogName;
+    this.createdAt = new Date().toISOString();
+    this.likesCount = 0;
+    this.dislikesCount = 0;
+    this.usersEngagement = [];
+  }
+}
---
+++ src/posts/posts-controller.ts
@@ -0,0 +1,99 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { PostsService } from './posts-service';
+import { PostsQueryRepository } from './posts-query-repository';
+import { CustomException } from '../blogs/functions/custom-exception';
+import { isValid } from '../blogs/functions/isValid-Id';
+import { QueryPaginationType } from '../blogs/blogs.controller';
+
+@Controller('posts')
+export class PostsController {
+  constructor(
+    protected postsService: PostsService,
+    protected postsQueryRepositories: PostsQueryRepository,
+  ) {}
+
+  @Post()
+  @HttpCode(201)
+  async createPost(@Body() inputModel: CreatePostInputModelType) {
+    const createdPostId = await this.postsService.createPost(
+      inputModel.title,
+      inputModel.shortDescription,
+      inputModel.content,
+      inputModel.blogId,
+    );
+
+    if (!createdPostId) {
+      throw new CustomException('Post cant be created', HttpStatus.NOT_FOUND);
+    }
+
+    return await this.postsQueryRepositories.findPostById(createdPostId);
+  }
+
+  @Get()
+  async getAllPosts(@Query() queryPagination: QueryPaginationType) {
+    const foundPosts = await this.postsQueryRepositories.findPosts(
+      null,
+      queryPagination,
+    );
+    return foundPosts;
+  }
+
+  @Get(':id')
+  async getPostById(@Param('id') id: string) {
+    console.log(id, 'id ');
+    isValid(id);
+    const findPostWithoutUserInfo =
+      await this.postsQueryRepositories.findPostByIdWithoutUser(id);
+
+    console.log(findPostWithoutUserInfo, 'findPostWithoutUserInfo');
+
+    if (!findPostWithoutUserInfo) {
+      throw new CustomException('Post not found', HttpStatus.NOT_FOUND);
+    }
+    return findPostWithoutUserInfo;
+  }
+
+  @Put(':id')
+  async updatePostById(
+    @Param('id') postId: string,
+    @Body() inputModel: CreatePostInputModelType,
+  ) {
+    isValid(postId);
+    const updatedPostId = await this.postsService.updatePost(
+      postId,
+      inputModel,
+    );
+    if (!updatedPostId) {
+      throw new CustomException('Post not found', HttpStatus.NOT_FOUND);
+    }
+    return;
+  }
+
+  @Delete(':id')
+  async deletePostById(@Param('id') postId: string) {
+    isValid(postId);
+    const isDeleted = await this.postsService.deletePost(postId);
+    if (!isDeleted) {
+      throw new CustomException('Blog not found', HttpStatus.NOT_FOUND);
+    }
+    return;
+  }
+}
+
+export type CreatePostInputModelType = {
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
+};
