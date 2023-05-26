cc68ade1e0d9fd37605e4a0f465629c121e76617
Anna-Khobta <ana14i88@gmail.com>
2023-05-26 17:33:36 +0300

    work with auth/login

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,101 @@
+import {
+  Body,
+  Controller,
+  createParamDecorator,
+  ExecutionContext,
+  Get,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { UsersQueryRepository } from '../users/users.query.repository';
+import { CustomException } from '../blogs/functions/custom-exception';
+import { AuthService } from './auth.service';
+import { TokenService } from '../token/token.service';
+import { Response } from 'express';
+import { CurrentUserId } from '../decorators/current-user-id.param.decorator';
+import { JwtAuthGuard } from '../auth-guards/jwt-auth.guard';
+
+export type LoginUserInputModelType = {
+  loginOrEmail: string;
+  password: string;
+};
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    protected usersService: UsersService,
+    protected usersQueryRepository: UsersQueryRepository,
+    protected authService: AuthService,
+    protected tokenService: TokenService,
+  ) {}
+
+  @Post('login')
+  async loginUser(
+    @Body() inputModel: LoginUserInputModelType,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    let ip;
+    let deviceTitle;
+
+    const IpUserAgent = createParamDecorator(
+      (data: unknown, context: ExecutionContext) => {
+        const request = context.switchToHttp().getRequest();
+        ip = request.ip;
+        deviceTitle = request.headers['user-agent'] || 'defaultDevice';
+        return { ip, deviceTitle };
+      },
+    );
+
+    console.log(ip, deviceTitle);
+
+    const foundUserInDb =
+      await this.usersQueryRepository.findUserByLoginOrEmail(
+        inputModel.loginOrEmail,
+        inputModel.loginOrEmail,
+      );
+
+    if (!foundUserInDb) {
+      throw new CustomException('No such user in app', HttpStatus.UNAUTHORIZED);
+    }
+
+    const isPasswordCorrect = await this.usersService.checkPasswordCorrect(
+      foundUserInDb.accountData.hashPassword,
+      inputModel.password,
+    );
+
+    if (!isPasswordCorrect) {
+      throw new CustomException(
+        'Incorrect login or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const loginUser = await this.authService.login(
+      foundUserInDb._id.toString(),
+    );
+
+    const isTokenAddedToDb = await this.tokenService.createTokenDB(
+      loginUser.decodedRefreshToken,
+      ip,
+      deviceTitle,
+    );
+
+    response.cookie('refreshToken', loginUser.refreshToken);
+    return { accessToken: loginUser.accessToken };
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getInfoAboutMeUser(@CurrentUserId() currentUserId: string) {
+    const meUser = await this.usersQueryRepository.findUserById(currentUserId);
+
+    return {
+      userId: meUser?.id,
+      login: meUser?.login,
+      email: meUser?.email,
+    };
+  }
+}
