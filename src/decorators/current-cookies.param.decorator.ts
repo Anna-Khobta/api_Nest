726483ac57243ff4d135c8b758ea9392b4b4a2bf
Anna-Khobta <ana14i88@gmail.com>
2023-06-01 00:58:38 +0300

    create, work

---
+++ src/decorators/current-cookies.param.decorator.ts
@@ -0,0 +1,16 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Cookies = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    //await jwtService.getRefreshTokenFromDb(refreshToken);
+    return data ? request.cookies?.[data] : request.cookies;
+  },
+);
+
+export const JwtPayload = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.user;
+  },
+);
