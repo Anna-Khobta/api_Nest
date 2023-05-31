c8f9127b69edb2a47bf3a774a1d2adeaddee01e4
Anna-Khobta <ana14i88@gmail.com>
2023-06-01 01:04:42 +0300

    create jwt strategy, it works

---
+++ src/auth-guards/jwt-refresh.strategy.ts
@@ -0,0 +1,29 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from './constants';
+import { Request } from 'express';
+
+const cookieExtractor = (req: Request) => {
+  const token = req.cookies['refreshToken'];
+  if (!token) return null;
+  return token;
+};
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: any) {
+    return payload;
+  }
+}
