302e9b1705bf5de0e5b7ba911b2211035d843ebf
Anna-Khobta <ana14i88@gmail.com>
2023-06-03 23:17:55 +0300

    updateUserLikeInfo without postInstance

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -7,6 +7,7 @@ import { LikeStatusesEnum, UserLikeInfo } from '../blogs/types';
 @Injectable()
 export class PostsRepository {
   constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
+
   async save(postInstance: PostDocument): Promise<boolean> {
     try {
       await postInstance.save();
@@ -107,27 +108,25 @@ export class PostsRepository {
     };
 
     try {
-      const postInstance = await this.postModel.findOne({
-        _id: postId,
-        'usersEngagement.userId': userLikeInfo.userId,
-      });
+      const updateResult = await this.postModel.updateOne(
+        {
+          _id: postId,
+          'usersEngagement.userId': userLikeInfo.userId,
+        },
+        {
+          $set: {
+            'usersEngagement.$.userStatus': likeStatus,
+            likesCount: likes,
+            dislikesCount: dislikes,
+          },
+        },
+      );
 
-      if (!postInstance) {
+      if (updateResult.matchedCount === 1) {
+        return true;
+      } else {
         return false;
       }
-
-      postInstance.usersEngagement.find(
-        (el) => el.userId === userLikeInfoToAdd.userId,
-      )!.userStatus = likeStatus;
-      postInstance.likesCount = likes;
-      postInstance.dislikesCount = dislikes;
-
-      await postInstance.save();
-
-      /*const postInstanceTEST = await PostModelClass.findOne({_id: postId})
-             console.log(postInstanceTEST)*/
-
-      return true;
     } catch (error) {
       console.log(error);
       return false;
@@ -135,130 +134,168 @@ export class PostsRepository {
   }
 }
 
+/*try {
+    const postInstance: PostDocument = await this.postModel.findOne({
+      _id: postId,
+      'usersEngagement.userId': userLikeInfo.userId,
+    });
+
+    if (!postInstance) {
+      return false;
+    }
+
+    const condition = (el) => el.userId === userLikeInfoToAdd.userId;
+    const foundUser = postInstance.usersEngagement.find(condition);
+
+    if (foundUser) {
+      foundUser.userStatus = likeStatus;
+    }
+
+    /!* const postInstLikeStat = (postInstance.usersEngagement.find(
+            (el) => el.userId === userLikeInfoToAdd.userId,
+          )!.userStatus = likeStatus);*!/
+
+    //console.log(postInstLikeStat, ' postInstLikeStat');
+
+    postInstance.likesCount = likes;
+    postInstance.dislikesCount = dislikes;
+
+    console.log(postInstance, 'postInstance');
+
+    await postInstance.save();
+
+    return true;*!/
+  } catch (error) {
+       console.log(error);
+       return false;
+     }
+   }
+ }*/
+
 //export const postsRepositories = new PostsRepository()
 
 /*
-export const postsRepositories = {
+  export const postsRepositories = {
 
-    async save(postInstance: HydratedDocument<PostDbType>): Promise<boolean> {
-        try {
-            await postInstance.save()
-            return true
-        } catch (error) {
-            console.log(error)
-            return false
-        }
+      async save(postInstance: HydratedDocument<PostDbType>): Promise<boolean> {
+          try {
+              await postInstance.save()
+              return true
+          } catch (error) {
+              console.log(error)
+              return false
+          }
 
-    },
+      },
 
-    async updatePost(postId: string, title: string, shortDescription: string, content: string): Promise<string | null> {
+      async updatePost(postId: string, title: string, shortDescription: string, content: string): Promise<string | null> {
 
-        const postInstance = await PostModelClass.findOne({_id: postId})
+          const postInstance = await PostModelClass.findOne({_id: postId})
 
-        if (!postInstance) {
-            return null
-        }
+          if (!postInstance) {
+              return null
+          }
 
-        //postInstance.updatePost()
+          //postInstance.updatePost()
 
-        postInstance.title = title;
-        postInstance.shortDescription = shortDescription;
-        postInstance.content = content;
+          postInstance.title = title;
+          postInstance.shortDescription = shortDescription;
+          postInstance.content = content;
 
-        try {
-            await postInstance.save()
-            return postInstance._id.toString()
-        } catch (error) {
-            console.log(error)
-            return null
-        }
+          try {
+              await postInstance.save()
+              return postInstance._id.toString()
+          } catch (error) {
+              console.log(error)
+              return null
+          }
 
-    },
-
-
-    async deletePost(id: string): Promise<boolean> {
-        const result = await PostModelClass.findOneAndDelete({_id: id})
-        return result !== null
-
-    },
-
-    async deleteAllPosts(): Promise<number> {
-        const result = await PostModelClass.deleteMany({})
-        return result.deletedCount
-    },
+      },
 
 
-    async createUserLikeInfoInDb(postId: string, userLikeInfo: UserLikeInfo, likeStatus: LikeStatusesEnum, likes: number, dislikes: number): Promise<boolean> {
+      async deletePost(id: string): Promise<boolean> {
+          const result = await PostModelClass.findOneAndDelete({_id: id})
+          return result !== null
 
-        let userLikeInfoToAdd: UserLikeInfo = {
-            userId: userLikeInfo.userId,
-            createdAt: userLikeInfo.createdAt,
-            userStatus: likeStatus
-        }
+      },
 
-        if (likeStatus === LikeStatusesEnum.Like) {
-            likes++
-        }
-
-        if (likeStatus === LikeStatusesEnum.Dislike) {
-            dislikes++
-        }
-
-        try {
-            const postInstance = await PostModelClass.findOne({_id: postId})
-
-            if (!postInstance) {
-                return false
-            }
-
-            postInstance.likesCount = likes;
-            postInstance.dislikesCount = dislikes;
-            postInstance.usersEngagement.push(userLikeInfoToAdd)
-
-            await postInstance.save();
-            return true
-        } catch (error) {
-            console.log(error)
-            return false
-        }
-    },
-
-    async updateUserLikeInfo(postId: string, userLikeInfo: UserLikeInfo, likeStatus: LikeStatusesEnum,
-                             likes: number, dislikes: number): Promise<boolean> {
-
-        let userLikeInfoToAdd: UserLikeInfo = {
-            userId: userLikeInfo.userId,
-            createdAt: userLikeInfo.createdAt,
-            userStatus: likeStatus
-        }
-
-        try {
-            const postInstance = await PostModelClass.findOne({
-                _id: postId,
-                "usersEngagement.userId": userLikeInfo.userId
-            })
+      async deleteAllPosts(): Promise<number> {
+          const result = await PostModelClass.deleteMany({})
+          return result.deletedCount
+      },
 
 
-            if (!postInstance) {
-                return false
-            }
+      async createUserLikeInfoInDb(postId: string, userLikeInfo: UserLikeInfo, likeStatus: LikeStatusesEnum, likes: number, dislikes: number): Promise<boolean> {
 
-            postInstance.usersEngagement.find(el => el.userId === userLikeInfoToAdd.userId)!.userStatus = likeStatus
-            postInstance.likesCount = likes;
-            postInstance.dislikesCount = dislikes;
+          let userLikeInfoToAdd: UserLikeInfo = {
+              userId: userLikeInfo.userId,
+              createdAt: userLikeInfo.createdAt,
+              userStatus: likeStatus
+          }
+
+          if (likeStatus === LikeStatusesEnum.Like) {
+              likes++
+          }
+
+          if (likeStatus === LikeStatusesEnum.Dislike) {
+              dislikes++
+          }
+
+          try {
+              const postInstance = await PostModelClass.findOne({_id: postId})
+
+              if (!postInstance) {
+                  return false
+              }
+
+              postInstance.likesCount = likes;
+              postInstance.dislikesCount = dislikes;
+              postInstance.usersEngagement.push(userLikeInfoToAdd)
+
+              await postInstance.save();
+              return true
+          } catch (error) {
+              console.log(error)
+              return false
+          }
+      },
+
+      async updateUserLikeInfo(postId: string, userLikeInfo: UserLikeInfo, likeStatus: LikeStatusesEnum,
+                               likes: number, dislikes: number): Promise<boolean> {
+
+          let userLikeInfoToAdd: UserLikeInfo = {
+              userId: userLikeInfo.userId,
+              createdAt: userLikeInfo.createdAt,
+              userStatus: likeStatus
+          }
+
+          try {
+              const postInstance = await PostModelClass.findOne({
+                  _id: postId,
+                  "usersEngagement.userId": userLikeInfo.userId
+              })
 
 
-            await postInstance.save();
+              if (!postInstance) {
+                  return false
+              }
 
-            /!*const postInstanceTEST = await PostModelClass.findOne({_id: postId})
-             console.log(postInstanceTEST)*!/
+              postInstance.usersEngagement.find(el => el.userId === userLikeInfoToAdd.userId)!.userStatus = likeStatus
+              postInstance.likesCount = likes;
+              postInstance.dislikesCount = dislikes;
 
-            return true
 
-        } catch (error) {
-            console.log(error)
-            return false
-        }
-    }
+              await postInstance.save();
 
-}*/
+              /!*const postInstanceTEST = await PostModelClass.findOne({_id: postId})
+               console.log(postInstanceTEST)*!/
+
+              return true
+
+          } catch (error) {
+              console.log(error)
+              return false
+          }
+      }
+
+  }*/
