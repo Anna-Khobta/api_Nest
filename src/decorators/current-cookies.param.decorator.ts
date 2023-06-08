94a0d55bff399550bddb6887f9e04a41dcea45e0
Anna-Khobta <ana14i88@gmail.com>
2023-06-08 16:43:38 +0300

    refactoring

--- src/decorators/current-cookies.param.decorator.ts
+++ src/decorators/current-cookies.param.decorator.ts
@@ -7,10 +7,3 @@ export const Cookies = createParamDecorator(
     return data ? request.cookies?.[data] : request.cookies;
   },
 );
-
-export const JwtPayload = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.user;
-  },
-);
--- src/users/users-input-model-class.ts
+++ src/users/users-input-model-class.ts
@@ -1,10 +1,14 @@
-import { IsEmail, Length } from 'class-validator';
+import { IsEmail, Length, Validate } from 'class-validator';
+import { IsEmailAlreadyExist } from '../decorators/IsEmailAlreadyExist.decorator';
+import { IsLoginAlreadyExist } from '../decorators/IsLoginAlreadyExist.validator';
 
 export class CreateUserInputModelClass {
   @Length(3, 10)
+  //@Validate(IsLoginAlreadyExist)
   login: string;
   @Length(6, 20)
   password: string;
   @IsEmail()
+  //@Validate(IsEmailAlreadyExist)
   email: string;
 }
