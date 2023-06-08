5ccaad6c1c464d8bf19dffb8561b5606ffd28205
Anna-Khobta <ana14i88@gmail.com>
2023-06-08 16:29:51 +0300

    create schema for db

---
+++ src/comments/comments-schema.ts
@@ -0,0 +1,77 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { LikeStatusesEnum } from '../blogs/types';
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema()
+export class CommentatorInfo {
+  @Prop({
+    required: true,
+  })
+  userId: string;
+  @Prop({
+    required: true,
+  })
+  @Prop()
+  userLogin: string;
+}
+
+@Schema()
+export class UsersEngagement {
+  @Prop()
+  userId: string;
+  @Prop()
+  createdAt: string;
+  @Prop({
+    required: true,
+  })
+  @Prop()
+  userStatus: LikeStatusesEnum;
+}
+
+@Schema()
+export class UserLikeInfo {
+  @Prop({
+    required: true,
+  })
+  userId: string;
+  @Prop({
+    required: true,
+  })
+  reatedAt: string;
+  @Prop({
+    required: true,
+  })
+  userStatus: LikeStatusesEnum;
+}
+
+@Schema()
+export class Comment {
+  @Prop({
+    required: true,
+  })
+  postId: string;
+  @Prop({
+    required: true,
+  })
+  content: string;
+  @Prop({
+    required: true,
+  })
+  createdAt: string;
+  @Prop()
+  commentatorInfo: CommentatorInfo;
+  @Prop({
+    required: true,
+  })
+  likesCount: number;
+  @Prop({
+    required: true,
+  })
+  dislikesCount: number;
+  @Prop({ default: [] })
+  usersEngagement: UsersEngagement[];
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
