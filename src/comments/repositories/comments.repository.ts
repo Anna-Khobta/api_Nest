fff14e729c49bfb7fa2e8b16f18db37ffea30032
Anna-Khobta <ana14i88@gmail.com>
2023-06-14 01:54:12 +0300

    test commentModel.aggregate

--- src/comments/repositories/comments.repository.ts
+++ src/comments/repositories/comments.repository.ts
@@ -171,18 +171,35 @@ export class CommentsRepository {
     );
 
     const commentEngagementUsersLikedAndBanned =
-      await this.commentModel.findOne(
+      await this.commentModel.aggregate([
         {
-          _id: commentId,
-          usersEngagement: {
-            $elemMatch: {
-              userId: { $in: bannedUserIds },
-              userStatus: LikeStatusesEnum.Like,
+          $match: {
+            usersEngagement: {
+              $elemMatch: {
+                userId: { $in: bannedUserIds },
+                userStatus: 'Like',
+              },
             },
           },
         },
-        { usersEngagement: 1 },
-      );
+        {
+          $project: {
+            userCount: {
+              $size: {
+                $filter: {
+                  input: '$usersEngagement',
+                  cond: {
+                    $and: [
+                      { $in: ['$$this.userId', bannedUserIds] },
+                      { $eq: ['$$this.userStatus', 'Like'] },
+                    ],
+                  },
+                },
+              },
+            },
+          },
+        },
+      ]);
 
     /*(
       {
@@ -195,10 +212,14 @@ export class CommentsRepository {
         ],
       },
       { usersEngagement: 1 },
-    )*/ console.log(
+    )*/
+
+    console.log(
       commentEngagementUsersLikedAndBanned,
       ' commentEngagementUsersLikedAndBanned ',
     );
+
+    //console.log(commentEngagementUsersLikedAndBanned[0].userCount);
 
     const commentEngagementUsersDislikedAndBanned =
       await this.commentModel.findOne(
@@ -214,15 +235,34 @@ export class CommentsRepository {
         { usersEngagement: 1 },
       );
 
-    const minusLikes =
-      commentEngagementUsersLikedAndBanned?.usersEngagement.length;
-    const minusDislikes =
-      commentEngagementUsersDislikedAndBanned?.usersEngagement.length;
+    let minusLikes;
+    if (!commentEngagementUsersLikedAndBanned[0]) {
+      minusLikes = 0;
+    } else {
+      minusLikes = commentEngagementUsersLikedAndBanned[0].userCount;
+    }
+
+    /*let minusLikes;
+    try {
+      minusLikes = commentEngagementUsersLikedAndBanned[0].userCount;
+    } catch (err) {
+      minusLikes = 0;
+    }*/
 
     //commentEngagementUsersLikedAndBanned[0]?.usersEngagement.length;
 
+    console.log(minusLikes, ' minusLikes');
+
+    const minusDislikes =
+      commentEngagementUsersDislikedAndBanned?.usersEngagement.length;
+
+    //commentEngagementUsersLikedAndBanned?.usersEngagement.length;
+    // commentEngagementUsersLikedAndBanned[0]?.usersEngagement.length;
+
     const likesCountWithBanned = comment.likesCount - minusLikes;
     const dislikesCountWithBanned = comment.dislikesCount - minusDislikes;
+
+    console.log(likesCountWithBanned);
 
     return {
       likesCountWithBanned: likesCountWithBanned,
