00742681dcb2c89f39e6c4eb4e69c6467ed3195e
Anna-Khobta <ana14i88@gmail.com>
2023-06-21 05:02:11 +0300

    refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Blog, BlogSchema } from './blogs/db/blogs-schema';
 import { BlogsService } from './blogs/blogs.service';
 import { BlogsController } from './blogs/apis/api/blogs.controller';
-import { BlogsQueryRepository } from './blogs/repositories/blogs.query.repository';
 import { Post, PostSchema } from './posts/posts-schema';
 import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
@@ -50,7 +49,6 @@ import { CreateUserUseCase } from './users/apis/sa-api/use-cases/create-user-use
 import { CqrsModule } from '@nestjs/cqrs';
 import { BanUserUseCase } from './users/apis/sa-api/use-cases/ban-user-use-case';
 import { BindBlogWithUserUseCase } from './blogs/apis/sa-api/sa-blogs.use.cases/bind-blog-with-user-use-case';
-import { UsersRepository } from './users/users-repositories/users.repository';
 import { SaBlogsController } from './blogs/apis/sa-api/sa-blogs.controller';
 import { LoginPasswordGuard } from './auth-guards/login-password.guard';
 import { BloggerBlogsController } from './blogs/apis/blogger-api/blogger-blogs.controller';
@@ -64,6 +62,8 @@ import { DeleteBlogByBloggerUseCase } from './blogs/apis/blogger-api/blogger-blo
 import { BanBlogBySaUseCase } from './blogs/apis/sa-api/sa-blogs.use.cases/ban-blog-by-sa-use-case';
 import { BanUserByBloggerUseCase } from './users/apis/blogger-api/blogger-user.use.cases/ban-user-by-blogger-use-case';
 import { BloggerUsersController } from './users/apis/blogger-api/blogger-users.controller';
+import { UsersRepository } from './users/users-repositories/users.repository';
+import { BlogsQueryRepository } from './blogs/repositories/blogs.query.repository';
 export const configModule = ConfigModule.forRoot({
   isGlobal: true,
   envFilePath: '.env',
