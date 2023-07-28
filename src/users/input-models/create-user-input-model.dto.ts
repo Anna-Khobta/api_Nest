9ffbe33d9117b2d986079838d8c3bc0be6bba3e7
Anna-Khobta <ana14i88@gmail.com>
2023-07-28 15:12:20 +0300

    refactoring

--- src/users/input-models/create-user-input-model.dto.ts
+++ src/users/input-models/create-user-input-model.dto.ts
@@ -1,12 +1,14 @@
-import { IsEmail, Length } from 'class-validator';
+import { IsEmail, Length, Validate } from 'class-validator';
+import { IsLoginAlreadyExist } from '../../decorators/IsLoginAlreadyExist.validator';
+import { IsEmailAlreadyExist } from '../../decorators/IsEmailAlreadyExist.decorator';
 
 export class CreateUserInputModel {
   @Length(3, 10)
-  //@Validate(IsLoginAlreadyExist)
+  @Validate(IsLoginAlreadyExist)
   login: string;
   @Length(6, 20)
   password: string;
   @IsEmail()
-  //@Validate(IsEmailAlreadyExist)
+  @Validate(IsEmailAlreadyExist)
   email: string;
 }
